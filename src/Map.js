fcd37041d3ca384a90836905f1c71078ab943b1a
anarchaworld <anarchaworld@gmail.com>
2023-03-04 18:53:36 -0800

    fix attempt

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
-import mapboxgl from "mapbox-gl";
+import mapboxgl from "!mapbox-gl";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
