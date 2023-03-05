7d6d6ef646f6d43d04bed94475f5530f1a2fce22
anarchaworld <anarchaworld@gmail.com>
2023-03-04 18:39:15 -0800

    Closed #3

--- src/Login.js
+++ src/Login.js
@@ -1,55 +1,49 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 function Login(props) {
-    const [person, setPerson] = useState(
-       {
-          email: "",
-          password: "",
-       }
-    );
+  const [person, setPerson] = useState({
+    email: "",
+    password: "",
+  });
 
-    function handleChange(event) {
-      const { name, value } = event.target;
-      if (name === "password")
-        setPerson(
-           {email: person['email'], password: value}
-        );
-      else     
-         setPerson(
-           {email: value, password: person['password']}   
-         );
-    }
-
-    function submitForm() {
-      console.log("Submit Form Person: "+person);
-      props.handleSubmit(person);
-      setPerson({email: '', password: ''});
-
-    }
-
-    return (
-      <form>
-        <label htmlFor="name">Email</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          value={person.email}
-          onChange={handleChange} />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={person.password}
-          onChange={handleChange} />
-          <input type="button" value="Submit" onClick={submitForm} />
-          <li>
-            <a href="/createaccount">Create Account</a>
-          </li>  
-      </form>
-      //Add link Need an account
-    );
+  function handleChange(event) {
+    const { name, value } = event.target;
+    if (name === "password")
+      setPerson({ email: person["email"], password: value });
+    else setPerson({ email: value, password: person["password"] });
   }
 
-  export default Login;
+  function submitForm() {
+    console.log("Submit Form Person: " + person);
+    props.handleSubmit(person);
+    setPerson({ email: "", password: "" });
+  }
+
+  return (
+    <form>
+      <label htmlFor="name">Email</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={person.email}
+        onChange={handleChange}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        value={person.password}
+        onChange={handleChange}
+      />
+      <input type="button" value="Submit" onClick={submitForm} />
+      <li>
+        <a href="/users/signup">Create Account</a>
+      </li>
+    </form>
+    //Add link Need an account
+  );
+}
+
+export default Login;
